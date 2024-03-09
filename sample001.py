e418ab10f403e7da201e14bee4e64fcab292bfe3
kokubuy <kokubuy@codegear.co.jp>
2024-03-09 15:36:02 +0900

    Add sample001.py

---
+++ sample001.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# coding=utf-8
+
+import sys
+
+#
+# https://pythonhosted.org/pyserial/shortintro.html
+#
+import serial
+
+def main():
+    ser = serial.Serial(                \
+        port = '/dev/ttyUSB0',          \
+        baudrate = 9600,                \
+        bytesize = serial.EIGHTBITS,    \
+        parity = serial.PARITY_NONE,    \
+        stopbits = serial.STOPBITS_ONE, \
+        timeout = 0,        \
+        write_timeout = 1,  \
+        xonxoff = False,    \
+        rtscts = True,      \
+        dsrdtr = False,     \
+        )
+    ser.write(b'hello')
+    return 0
+
+if __name__ == '__main__':
+    # execute only if run as a script
+    sys.exit(main())
