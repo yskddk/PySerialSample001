fd4fd4a5bfb99e4d6767ae92e044c3301c221f2e
kokubuy <kokubuy@codegear.co.jp>
2024-03-13 14:31:49 +0900

    Modify, sample002
     * list up the COM ports
     * add CR detectable readline function

--- sample002.py
+++ sample002.py
@@ -1,41 +1,85 @@
 #!/usr/bin/env python3
-# -*- coding: utf8 -*-
+# -*- coding: utf-8 -*-
 
 import sys
 import io
-
-#
-# https://pythonhosted.org/pyserial/shortintro.html
-#
 import serial
+import serial.tools.list_ports
+
+
+
+def readline_cr_(ser_):
+    """
+    This version of the readline() treats newline as CR.
+    It'll return bytes object.
+    Referenced to https://stackoverflow.com/questions/16470903/ .
+    """
+    cr = b'\r'
+    line = bytearray()
+
+    while True:
+        c_ = ser_.read(1)
+        if not c_:
+            line = bytearray() #clear
+            break
+
+        line += c_
+        if c_ == cr:
+            break
+
+    return bytes(line)
+
+
 
 def main():
-    ## ser = serial.serial_for_url(        \
-    ##     url = '/dev/ttyUSB0',           \
+
+    # Listup established serial ports on this PC
+    # Referenced to https://pythonhosted.org/pyserial/shortintro.html
+
+    found_ports_ = {}
+    for i,port in enumerate(serial.tools.list_ports.comports()):
+        print('Serial port found: [{}] {}'.format(i, port.device))
+        found_ports_[str(i)] = port.device
+
+    if (0 == len(found_ports_)):
+        print('No serial ports found.')
+        return 0
+
+    port_name_ = found_ports_['0']
+    print('Use the {}'.format(port_name_))
+
     ser = serial.Serial(                \
-        port = '/dev/ttyUSB0',          \
+        port = port_name_,              \
         baudrate = 9600,                \
         bytesize = serial.EIGHTBITS,    \
         parity = serial.PARITY_NONE,    \
         stopbits = serial.STOPBITS_ONE, \
-        timeout = 10,        \
-        write_timeout = 1,  \
-        xonxoff = False,    \
-        rtscts = True,      \
-        dsrdtr = False,     \
+        write_timeout = 0.5,            \
+        timeout = 0.5,                  \
+        xonxoff = False,                \
+        rtscts = True,                  \
+        dsrdtr = False,                 \
         )
-    ## sio = io.TextIOWrapper(io.BufferedRWPair(ser, ser));
 
-    print('CTS = %s'%str(ser.cts))
-    print('RTS = %s'%str(ser.rts))
+    cmd = b'AXIsX:READY?\r'
+    #cmd = b'*ESR?\r'
+    print('CTS:{}/RTS:{}, before write, send {}'.format(ser.cts, ser.rts, cmd))
 
-    ser.write(b'hello\r')
+    ser.write(cmd)
 
-    ## line = sio.readline()
-    line= ser.read()
-    print(line)
+    print('CTS:{}/RTS:{}, after write'.format(ser.cts, ser.rts))
+
+    resp = readline_cr_(ser)
+    rstr = resp.decode('ascii').strip('\r\n')
+
+    if 0 == len(rstr):
+        print('CTS:{}/RTS:{}, error'.format(ser.cts, ser.rts))
+    else:
+        print('CTS:{}/RTS:{}, received {}'.format(ser.cts, ser.rts, rstr))
 
     return 0
+
+
 
 if __name__ == '__main__':
     # execute only if run as a script
